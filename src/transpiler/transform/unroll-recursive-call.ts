9eae1ef3456dafcb51cf5a74a386d98259da54dc
Andreas Madsen <amwebdk@gmail.com>
2019-06-28 21:23:44 +0200

    unroll 2nd order indirect recursive calls

--- src/transpiler/transform/unroll-recursive-call.ts
+++ src/transpiler/transform/unroll-recursive-call.ts
@@ -1,103 +1,125 @@
 
 import { FileAST, FuncDef, Node, FuncCall, Constant, Decl, FuncDecl } from '../ast';
 
-function isRecursiveFunCall(func: FuncDef, child: Node): child is FuncCall {
-    return (child instanceof FuncCall &&
-            child.name.name === func.decl.name);
+function dublicateFunction(func: FuncDef, newName: string): FuncDef {
+    if (!(func.decl.type instanceof FuncDecl)) {
+        throw new Error('unexpected declaration type');
+    }
+
+    return new FuncDef({
+        _nodetype: 'FuncDef',
+        coord: 'transform/unroll-recursive-call.js',
+        decl: {
+            _nodetype: 'Decl',
+            coord: 'transform/unroll-recursive-call.js',
+            bitsize: func.decl.bitsize,
+            funcspec: func.decl.funcspec,
+            init: func.decl.init,
+            name: newName,
+            quals: func.decl.quals,
+            storage: func.decl.storage,
+            type: {
+                _nodetype: 'FuncDecl',
+                coord: 'transform/unroll-recursive-call.js',
+                args: func.decl.type.args,
+                type: {
+                    _nodetype: 'TypeDecl',
+                    coord: 'transform/unroll-recursive-call.js',
+                    declname: newName,
+                    quals: func.decl.type.type.quals,
+                    type: func.decl.type.type.type
+                }
+            }
+        },
+        param_decls: func.param_decls,
+        body: func.body
+    });
+}
+
+function renameFunctionCalls(
+    func: FuncDef, fromName: string, toName: string
+): void {
+    func.transformChildren(
+        function transform<T extends Node>(child: T): T {
+            if (child instanceof FuncCall && child.name.name === fromName) {
+                child = new FuncCall({
+                    _nodetype: 'FuncCall',
+                    coord: 'transform/unroll-recursive-call.js',
+                    name: {
+                        _nodetype: 'ID',
+                        coord: 'transform/unroll-recursive-call.js',
+                        name: toName
+                    },
+                    args: child.args
+                }) as Node as T;
+            }
+            return child.transformChildren(transform);
+        }
+    );
+}
+
+function replaceFunctionCallWithNAN(func: FuncDef, name: string): FuncDef {
+    return func.transformChildren(
+        function transform<T extends Node>(child: T): T {
+            if (child instanceof FuncCall && child.name.name === name) {
+                child = new Constant({
+                    _nodetype: 'Constant',
+                    coord: 'transform/unroll-recursive-call.js',
+                    type: 'float',
+                    value: 'NAN'
+                }) as Node as T;
+            }
+            return child.transformChildren(transform);
+        }
+    );
 }
 
 export function unrollRecursiveCall(basename: string, ast: FileAST): FileAST {
     const insertFunctions: FuncDef[] = [];
+    const functionCalls = new Map<
+        string, { func: FuncDef, calls: Set<string> }
+    >();
 
+    // Create a basic graph (name -> calls[]) of all calls
     for (const func of ast.ext) {
         if (func instanceof FuncDef) {
-            const funcName = func.decl.name;
-
-            // Detect recursive calls
-            let detectedRecursiveCall = false;
+            const name = func.decl.name;
+            const calls = new Set<string>();
             func.transformChildren(
-                function transform<T extends Node>(child: T): T {
-                    if (isRecursiveFunCall(func, child)) {
-                        detectedRecursiveCall = true;
+                function scan<T extends Node>(child: T): T {
+                    if (child instanceof FuncCall) {
+                        calls.add(child.name.name);
                     }
-                    return child.transformChildren(transform);
+                    return child.transformChildren(scan);
                 }
             );
+            functionCalls.set(name, {func, calls});
+        }
+    }
 
-            if (detectedRecursiveCall) {
-                if (!(func.decl.type instanceof FuncDecl)) {
-                    throw new Error('unexpected declaration type');
+    // Check for recursive calls and replace recursive
+    // calls with NAN accordingly
+    for (const func of ast.ext) {
+        if (func instanceof FuncDef) {
+            const name = func.decl.name;
+            for (const callName of functionCalls.get(name).calls) {
+                // Check for 2-order recursive calls, root -> other -> root
+                if (functionCalls.has(callName) &&
+                    functionCalls.get(callName).calls.has(name)) {
+                    insertFunctions.push(
+                        replaceFunctionCallWithNAN(
+                            dublicateFunction(
+                                functionCalls.get(callName).func,
+                                `${name}_recusive_${callName}`),
+                            name));
+                    renameFunctionCalls(
+                        func, callName, `${name}_recusive_${callName}`);
                 }
-
-                const funcRecursive = new FuncDef({
-                    _nodetype: 'FuncDef',
-                    coord: 'transform/unroll-recursive-call.js',
-                    decl: {
-                        _nodetype: 'Decl',
-                        coord: 'transform/unroll-recursive-call.js',
-                        bitsize: func.decl.bitsize,
-                        funcspec: func.decl.funcspec,
-                        init: func.decl.init,
-                        name: `${basename}_${funcName}_recursive`,
-                        quals: func.decl.quals,
-                        storage: func.decl.storage,
-                        type: {
-                            _nodetype: 'FuncDecl',
-                            coord: 'transform/unroll-recursive-call.js',
-                            args: func.decl.type.args,
-                            type: {
-                                _nodetype: 'TypeDecl',
-                                coord: 'transform/unroll-recursive-call.js',
-                                declname: `${basename}_${funcName}_recursive`,
-                                quals: func.decl.type.type.quals,
-                                type: func.decl.type.type.type
-                            }
-                        }
-                    },
-                    param_decls: func.param_decls,
-                    body: func.body
-                });
-
-                // Replace recursive function call with an __recursive call
-                func.transformChildren(
-                    function transform<T extends Node>(child: T): T {
-                        if (isRecursiveFunCall(func, child)) {
-                            child = new FuncCall({
-                                _nodetype: 'FuncCall',
-                                coord: 'transform/unroll-recursive-call.js',
-                                name: {
-                                    _nodetype: 'ID',
-                                    coord: 'transform/unroll-recursive-call.js',
-                                    name: `${basename}_${funcName}_recursive`
-                                },
-                                args: child.args
-                            }) as Node as T;
-                        }
-                        return child.transformChildren(transform);
-                    }
-                );
-
-                // Replace recursive call with NAN constant
-                funcRecursive.transformChildren(
-                    function transform<T extends Node>(child: T): T {
-                        if (isRecursiveFunCall(func, child)) {
-                            child = new Constant({
-                                _nodetype: 'Constant',
-                                coord: 'transform/unroll-recursive-call.js',
-                                type: 'float',
-                                value: 'NAN'
-                            }) as Node as T;
-                        }
-                        return child.transformChildren(transform);
-                    }
-                );
-
-                // Mark funcRecursive for insertion into the ast
-                insertFunctions.push(funcRecursive);
             }
         }
     }
 
+    // Insert additional functions
     for (const func of insertFunctions) {
         ast.ext.push(func);
         ast.ext.unshift(new Decl(func.decl));
